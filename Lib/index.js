82761c410af513ddc3f31d12d2ea8db03733ebeb
SamAbdallah <saa081@student.bau.edu.lb>
2022-05-22 19:28:18 +0300

    Setting up the game function

--- Lib/index.js
+++ Lib/index.js
@@ -18,6 +18,40 @@ window.onload=function () {
         playerClick()
     };
 
+
+    document.addEventListener("keypress", x)
+
+
+    function game() {
+
+
+        var random = colors[Math.floor(Math.random() * colors.length)];
+        level += 1
+        document.getElementById("main").innerHTML = "Level" + level;
+        auto = document.getElementById(random)
+        fade(auto)
+
+
+        if (random=="red"){
+            red.play()
+        }
+        else if(random=="blue"){
+            blue.play()
+        }
+        else if(random=="yellow"){
+            yellow.play()
+        }
+        else if(random=="green"){
+            green.play()
+        }
+
+
+        store.push(random)
+        PlayerChoice = []
+
+
+    }
+
 }
 
 
